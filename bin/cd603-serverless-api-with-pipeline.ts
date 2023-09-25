db1772202917e5e88ff30a1bec89973169b23cb2
ariefh2-at-320740598534 <mr.arief.hidayat@gmail.com>
2023-09-26 05:50:18 +0700

    add gitignore list

--- bin/cd603-serverless-api-with-pipeline.ts
+++ bin/cd603-serverless-api-with-pipeline.ts
@@ -4,7 +4,10 @@ import { ServerlessApiStack } from '../lib/serverless-api-stack';
 import {PipelineStack} from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-const env: cdk.Environment = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+const env: cdk.Environment = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
+}
 new ServerlessApiStack(app, 'cd63-api-dev', {
   env: env,
   restResourceName: 'data',
